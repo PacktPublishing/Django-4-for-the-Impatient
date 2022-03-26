5a9826aab8c10d7f54860151f7d4d6fd5b49308d
Daniel Correa <yo@danielgara.com>
2022-03-26 08:23:54 -0500

    imp: Chapter03

--- Chapter03/moviereviewsproject/moviereviews/urls.py
+++ Chapter03/moviereviewsproject/moviereviews/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from movie import views as movieViews 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', movieViews.home),
+    path('about/', movieViews.about),
 ]
