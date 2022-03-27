679243dcb9869d92999555b1b2df98b25ae47ac4
Daniel Correa <yo@danielgara.com>
2022-03-27 14:56:22 -0500

    imp: Chapter04

--- Chapter04/moviereviewsproject/moviereviews/urls.py
+++ Chapter04/moviereviewsproject/moviereviews/urls.py
@@ -19,6 +19,7 @@ from movie import views as movieViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', movieViews.home),
-    path('about/', movieViews.about),
+    path('', movieViews.home, name='home'),
+    path('about/', movieViews.about, name='about'),
+    path('signup/', movieViews.signup, name='signup'),
 ]
