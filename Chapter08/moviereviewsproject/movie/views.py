93f34c78c7e6aacb215cb7e4930a79e19296567f
Daniel Correa <yo@danielgara.com>
2022-04-08 15:56:41 -0500

    imp: pre-chapter-08

---
+++ Chapter08/moviereviewsproject/movie/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Movie
+
+def home(request):
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html',	{'searchTerm':searchTerm, 'movies': movies})
+
+def about(request):
+    return HttpResponse('<h1>Welcome to About Page</h1>')
+
+def signup(request):
+    email = request.GET.get('email')
+    return render(request, 'signup.html', {'email':email})
