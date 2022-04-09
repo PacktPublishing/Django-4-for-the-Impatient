560de85fa62d010256c848d4a052a2cb9eccc94b
Daniel Correa <yo@danielgara.com>
2022-04-09 10:54:28 -0500

    imp: pre-chapter-11

---
+++ Chapter11/moviereviewsproject/accounts/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+from .forms import UserCreateForm
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
+from django.shortcuts import redirect
+from django.db import IntegrityError
+
+def signupaccount(request):
+    if request.method == 'GET':
+        return render(request, 'signupaccount.html', 
+                       {'form':UserCreateForm})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], 
+                            password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('home')
+            except IntegrityError:
+                return render(request, 'signupaccount.html', 
+                 {'form':UserCreateForm,
+                 'error':'Username already taken. Choose new username.'})
+        else:
+            return render(request, 'signupaccount.html', 
+             {'form':UserCreateForm, 'error':'Passwords do not match'})
+
+def logoutaccount(request):        
+    logout(request)
+    return redirect('home')
+
+def loginaccount(request):    
+    if request.method == 'GET':
+        return render(request, 'loginaccount.html', 
+                      {'form':AuthenticationForm})
+    else:
+        user = authenticate(request, username=request.POST['username'],
+                            password=request.POST['password'])
+        if user is None:
+            return render(request,'loginaccount.html', 
+                    {'form': AuthenticationForm(), 
+                    'error': 'username and password do not match'})
+        else: 
+            login(request,user)
+            return redirect('home')
---
+++ Chapter11/moviereviewsproject/moviereviews/urls.py
@@ -0,0 +1,32 @@
+"""moviereviews URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from movie import views as movieViews 
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', movieViews.home, name='home'),
+    path('about/', movieViews.about, name='about'),
+    path('signup/', movieViews.signup, name='signup'),
+    path('news/', include('news.urls')),
+    path('movie/', include('movie.urls')),
+    path('accounts/', include('accounts.urls')),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
